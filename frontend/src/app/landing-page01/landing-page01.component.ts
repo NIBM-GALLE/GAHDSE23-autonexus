18521bb07a6463abdea3dabe41568dc2c203447b
Kalana De Silva <141452020+Kalana0001@users.noreply.github.com>
2025-02-17 16:27:07 +0530

    Create Landing Page01

---
+++ frontend/src/app/landing-page01/landing-page01.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { NavbarLandingComponent } from '../landing-navbar/landing-navbar.component';
+
+@Component({
+  selector: 'app-landing-page01',
+  imports: [NavbarLandingComponent],
+  templateUrl: './landing-page01.component.html',
+  styleUrl: './landing-page01.component.css'
+})
+export class LandingPage01Component {
+
+}
